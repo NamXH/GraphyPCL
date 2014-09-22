ef4dbf0e308dec931b29688c779d2018173e0525
Nam Hoang <nam.xuan.hoang.work@gmail.com>
2014-09-22 15:42:47 -0600

    Add ExtendedTableViewRenderer for Android

---
+++ GraphyPCL.Android/CustomRenderers/ExtendedTableViewRenderer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Android.Widget;
+using GraphyPCL;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using GraphyPCL.Android;
+
+[assembly: ExportRenderer(typeof(ExtendedTableView), typeof(ExtendedTableViewRenderer))]
+namespace GraphyPCL.Android
+{
+    public class ExtendedTableViewRenderer : TableViewRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<TableView> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement == null)
+            {
+                ((ExtendedTableView)e.NewElement).DataChanged += (object sender, EventArgs args) =>
+                {
+                    ((TableViewModelRenderer)Control.Adapter).NotifyDataSetChanged();
+                };
+            }
+        }
+    }
+}
