10d3f13ed29d6bc3712c132666cf85b27a8c31fd
Nam Hoang <nam.xuan.hoang.work@gmail.com>
2015-04-12 16:01:09 -0600

    Add more icons

---
+++ GraphyPCL/Pages/TagSearchNavigationPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace GraphyPCL
+{
+    public class TagSearchNavigationPage : NavigationPage
+    {
+        public TagSearchNavigationPage()
+        {
+            Icon = "tag_icon.png";
+            Title = "Tags";
+            this.PushAsync(new Page());
+        }
+    }
+}
