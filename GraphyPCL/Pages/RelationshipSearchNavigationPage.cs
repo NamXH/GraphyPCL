af1e235430fc1626b6a3ea3b1673f5fe6d024826
Nam Hoang <nam.xuan.hoang.work@gmail.com>
2015-04-13 19:33:07 -0600

    Add RelationshipSearch Page

---
+++ GraphyPCL/Pages/RelationshipSearchNavigationPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace GraphyPCL
+{
+    public class RelationshipSearchNavigationPage : NavigationPage
+    {
+        public RelationshipSearchNavigationPage()
+        {
+            Icon = "genius_icon.png";
+            Title = "Relationships";
+            this.PushAsync(new RelationshipSearchPage());
+        }
+    }
+}
---
+++ GraphyPCL/Pages/RelationshipSearchPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace GraphyPCL
+{
+    public partial class RelationshipSearchPage : ContentPage
+    {
+        public RelationshipSearchPage()
+        {
+            InitializeComponent();
+            _tableView.Intent = TableIntent.Menu;
+        }
+    }
+}
