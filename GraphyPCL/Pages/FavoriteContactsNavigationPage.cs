e97ddac36ed42c9bf1d23acfd159df0097a60878
Nam Hoang <nam.xuan.hoang.work@gmail.com>
2015-06-15 12:19:01 -0600

    Fix bug when manipulating NavigationStack on Android

--- GraphyPCL/Pages/FavoriteContactsNavigationPage.cs
+++ GraphyPCL/Pages/FavoriteContactsNavigationPage.cs
@@ -6,7 +6,8 @@ namespace GraphyPCL
 {
     public class FavoriteContactsNavigationPage : NavigationPage
     {
-        public Page TopNavigationStack {
+        public Page TopNavigationStack
+        {
             get;
             set;
         }
@@ -25,13 +26,25 @@ namespace GraphyPCL
             base.OnAppearing();
 
             var favoriteContacts = DatabaseManager.GetRows<Contact>().Where(x => x.Favorite == true).ToList();
-            var favoritePage = new ContactsPage(favoriteContacts, false);
-            favoritePage.Title = "Favorite Contacts";
+            var favoritePage = new ContactsPage(favoriteContacts, false)
+            {
+                Title = "Favorite Contacts"
+            };
 
-            // Everytime, push a new page with updated favorite contacts to the navigation stack then remove the old one
-            this.PushAsync(favoritePage);
-            this.Navigation.RemovePage(TopNavigationStack); // Hide the animation when popsync()
-            TopNavigationStack = favoritePage;
+            Device.OnPlatform(
+                iOS: () => // Special implementation for better UX on iOS, does not work on Android
+                {
+                    // Everytime, push a new page with updated favorite contacts to the navigation stack then remove the old one
+                    this.PushAsync(favoritePage);
+                    this.Navigation.RemovePage(TopNavigationStack); // Do this to hide the animation when popsync()
+                    TopNavigationStack = favoritePage;
+                },
+                Default: () => // Simple implementation
+                {
+                    Navigation.PopAsync();
+                    Navigation.PushAsync(favoritePage);
+                }
+            );
         }
     }
 }
--- GraphyPCL/Pages/MainPage.xaml.cs
+++ GraphyPCL/Pages/MainPage.xaml.cs
@@ -10,9 +10,6 @@ namespace GraphyPCL
         {
             InitializeComponent();
             this.SelectedItem = _allContactsNavigationPage;
-
-            // Not showing Favourite Page to workaround bug: System.ArgumentException: 'jobject' must not be IntPtr.Zero
-            Device.OnPlatform(Android: () => this.Children.RemoveAt(0));
         }
     }
 }
