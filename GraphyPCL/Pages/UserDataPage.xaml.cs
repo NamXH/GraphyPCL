fa22db1d8a6fe676b907b8aa98296d861574d91d
Nam Hoang <nam.xuan.hoang.work@gmail.com>
2015-06-30 00:58:23 -0600

    Create summary page

---
+++ GraphyPCL/Pages/UserDataPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GraphyPCL
+{
+    public partial class UserDataPage : ContentPage
+    {
+        public UserDataPage()
+        {
+            InitializeComponent();
+            _tableView.Intent = TableIntent.Menu;
+
+            Title = "Summary";
+            Icon = "genius_icon.png";
+            BindingContext = UserDataManager.UserData;
+        }
+    }
+}
